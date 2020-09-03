12dcc8639e7c4d93ccbfbe25739ffdeda0fe7a76
jacoblovins <lovins.jacob@yahoo.com>
2020-09-03 06:13:16 -0400

    final functionality

--- queries.js
+++ queries.js
@@ -22,12 +22,12 @@ const allManagerSearch = () => {
 }
 
 const viewAllEmployees = () => {
-    const query = "SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.department_name, CONCAT(manager.first_name, ' ', manager.last_name) as manager FROM employees LEFT JOIN roles ON employees.role_id=roles.id LEFT JOIN departments ON roles.department_id = departments.dptid LEFT JOIN employees manager ON employees.manager_id=manager.id";
+    const query = "SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.department_name, CONCAT (manager.first_name, ' ', manager.last_name) as manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.dptid LEFT JOIN employees manager ON employees.manager_id = manager.id";
     return connection.query(query);
 }
 
 const viewEmployeesByDepartment = (department) => {
-    const query = "SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.department_name, CONCAT(manager.first_name, ' ', manager.last_name) as manager FROM employees LEFT JOIN roles ON employees.role_id=roles.id LEFT JOIN departments ON roles.department_id = departments.dptid LEFT JOIN employees manager ON employees.manager_id=manager.id WHERE departments.department_name = ?";
+    const query = "SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.department_name, CONCAT (manager.first_name, ' ', manager.last_name) as manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.dptid LEFT JOIN employees manager ON employees.manager_id = manager.id WHERE departments.department_name = ?";
     const dptName = [department]
     return connection.query(query, dptName);
 }
@@ -39,7 +39,7 @@ const viewEmployeesByManager = (manager) => {
     } else {
         managerName = [2];
     }
-    const query = "SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) as manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.dptid LEFT JOIN employees manager ON employees.manager_id=manager.id WHERE employees.manager_id = ?";
+    const query = "SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name, roles.salary, CONCAT (manager.first_name, ' ', manager.last_name) as manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.dptid LEFT JOIN employees manager ON employees.manager_id = manager.id WHERE employees.manager_id = ?";
     return connection.query(query, managerName);
 }
 
